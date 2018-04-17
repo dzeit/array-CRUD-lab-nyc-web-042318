50f50671d9e314902230255cc77fc74aae70e849
David Zeit <dzeit216@gmail.com>
2018-04-17 00:59:33 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Dolphins", "Yankees", "Knicks", "Rangers"]
+  my_teams = ["Dolphins", "Yankees", "Knicks", "Rangers"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  
 end
 
 def add_element_to_start_of_array(array, element)
