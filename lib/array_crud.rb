2db5c148eb503c1bec20c5f34cc04736fc93a5a0
David Zeit <dzeit216@gmail.com>
2018-04-17 01:05:23 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,25 +11,25 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  my_teams.unshift("Raiders")
 end
 
 def remove_element_from_end_of_array(array)
-
+  my_teams.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  my_teams.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  my_teams[1]
 end
 
 def retrieve_first_element_from_array(array)
-
+  my_teams[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  my_teams[3]
 end
