7a889cd22aca1956159d9faab51bb0fc58d6cedd
David Zeit <dzeit216@gmail.com>
2018-04-17 01:17:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Dolphins", "Yankees", "Knicks", "Rangers"]
+  ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "Suns"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("Raiders")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
